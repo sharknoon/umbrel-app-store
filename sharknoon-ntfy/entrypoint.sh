9f48a5f2b8c3cc58cb4df97a304391827cb16133
Josua Frank <frank.josua@gmail.com>
2024-06-02 13:12:34 +0200

    maybe

--- sharknoon-ntfy/entrypoint.sh
+++ sharknoon-ntfy/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ntfy serve &
 sleep 2
-# We need to use script to get rid of this error message: password: inappropriate ioctl for device
-script -O /dev/null -c "NTFY_PASSWORD=${NTFY_PASSWORD} ntfy user add --role=admin umbrel || true"
+NTFY_PASSWORD=${NTFY_PASSWORD} ntfy user add --role=admin umbrel || true
 wait
